31297b971a3b528b428951e2b9e58db9575b8425
Ezequiel Rindello <rindelloezequiel@gmail.com>
2025-02-13 19:52:40 -0300

    Refactor routes to enforce authentication and admin checks for adoption and user endpoints

--- routes/adoptions.js
+++ routes/adoptions.js
@@ -4,11 +4,9 @@ const router = express.Router();
 const adoptionController = require("../controllers/adoptionController.js");
 const { authenticate, isAdmin } = require("../middleware/autenticate.js");
 
-// Public routes
-router.get("/", adoptionController.getAll);
-router.get("/:id", adoptionController.getSingle);
-
 // Protected routes
+router.get("/", authenticate, isAdmin, adoptionController.getAll);
+router.get("/:id", authenticate, isAdmin, adoptionController.getSingle);
 router.post("/", authenticate, isAdmin, adoptionController.addAdoption);
 router.put("/:id", authenticate, isAdmin, adoptionController.updateAdoption);
 router.delete("/:id", authenticate, isAdmin, adoptionController.deleteAdoption);
--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,9 @@ const router = express.Router();
 const usersController = require("../controllers/usersController.js");
 const { authenticate, isAdmin } = require("../middleware/autenticate.js");
 
-// Public routes
-router.get("/", usersController.getAll);
-router.get("/:id", usersController.getSingle);
-
 // Protected routes
+router.get("/", authenticate, isAdmin, usersController.getAll);
+router.get("/:id", authenticate, isAdmin, usersController.getSingle);
 router.post("/", authenticate, isAdmin, usersController.addUser);
 router.put("/:id", authenticate, isAdmin, usersController.updateUser);
 router.delete("/:id", authenticate, isAdmin, usersController.deleteUser);
